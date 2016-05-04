17ab21acfc3402017555c88b864ee42e7f2af707
qwe <artyom366@inbox.lv>
2016-05-04 12:03:41 +0300

    Update file_search.cpp

--- file_search.cpp
+++ file_search.cpp
@@ -1,81 +1,68 @@
-#include <windows.h>
-#include <string.h>
-#include <stdio.h>
+#include<iostream>
+#include<windows.h>
 #include<tchar.h>
-#include <wchar.h>
 
-int recursive(HANDLE hfile, WIN32_FIND_DATA lpFile);
+using namespace std;
 
-int main(int argc, char* argv[])
+
+void main()
 {
-	HANDLE hFile;
+	HANDLE hFileResult;
+	hFileResult = CreateFile(_TEXT("files.dat."), GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	HANDLE hFileBase;
 	WIN32_FIND_DATA lpFile;
 	SYSTEMTIME lpSys;
+	SetCurrentDirectory(_TEXT("C:\\"));
+	hFileBase = FindFirstFile(_TEXT("*.*"), &lpFile);
 
-	int i = 0, j = 0;
+	SetFilePointer(hFileResult, 4, NULL, FILE_END);
 
-	char fileName[] = "a.txt";
+	if (hFileBase != INVALID_HANDLE_VALUE) {
 
-	SetCurrentDirectory(_TEXT("C:\\search"));
-	hFile = FindFirstFile(_TEXT("*.*"), &lpFile);
-
-	if (hFile != INVALID_HANDLE_VALUE)
-	{
 		if (lpFile.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
 		{
-			printf("DIRECTORY\t");
-			i++;
+			//printf("DIRECTORY\t");
 		}
 		else
 		{
-			printf("FILE\t\t");
-			j++;
+			//printf("FILE\t\t");
 		}
 
 		FileTimeToSystemTime(&lpFile.ftLastWriteTime, &lpSys);
-		printf_s("%ws\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
+		//printf_s("%ws\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
+
+		char buffer[50];
+		sprintf_s(buffer, "%ws\t\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
+
+		WriteFile(hFileResult, buffer, sizeof(buffer), NULL, NULL);
+
+
+	} 
+	else {
+		return;
 	}
-	else
-		printf("NO");
 
-	recursive(hFile, lpFile);
-
-	system("PAUSE");
-	return 0;
-}
-
-int recursive(HANDLE hFile, WIN32_FIND_DATA lpFile) {
-
-	SYSTEMTIME lpSys;
-
-	while (FindNextFile(hFile, &lpFile))
+	while (FindNextFile(hFileBase, &lpFile))
 	{
 		if (lpFile.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
 		{
 			printf("DIRECTORY\t");
 
-			wchar_t lpBuffer[MAX_COMPUTERNAME_LENGTH + 1];
-			DWORD nBufferLength = sizeof(lpBuffer);
-			GetCurrentDirectory(nBufferLength, lpBuffer);
-
-			wchar_t* newDirectory = wcsncat(lpBuffer, lpFile.cFileName, nBufferLength);
-			SetCurrentDirectory(newDirectory);
-
-			recursive(hFile, lpFile);
 		}
 		else
 		{
 			printf("FILE\t\t");
-
 		}
 
 		FileTimeToSystemTime(&lpFile.ftLastWriteTime, &lpSys);
+
 		printf_s("%ws\t\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
+
 	}
 
-	FindClose(hFile);
-	
-
+	FindClose(hFileBase);
 	system("PAUSE");
-	return 0;
 }
+}
+
