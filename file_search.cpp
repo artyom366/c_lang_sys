7f89c3f5f771b0c39f35ed8f56c581de51cbb3ce
qwe <artyom366@inbox.lv>
2016-05-06 21:12:26 +0300

    Update file_search.cpp

--- file_search.cpp
+++ file_search.cpp
@@ -10,15 +10,23 @@ int main()
 	SetCurrentDirectory(_TEXT("C:\\"));
 
 	//find first file
-	SYSTEMTIME lpSys;
+	SYSTEMTIME creationTime;
+	SYSTEMTIME accessTime;
+	SYSTEMTIME modificationTime;
+
 	WIN32_FIND_DATA lpFile;
 	HANDLE hFileMask = FindFirstFile(_TEXT("*.*"), &lpFile);
 
-	FileTimeToSystemTime(&lpFile.ftLastWriteTime, &lpSys);
-
 	//write first file 
-	char buffer[50];
-	sprintf_s(buffer, "%ws\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
+	char buffer[100];
+	FileTimeToSystemTime(&lpFile.ftCreationTime, &creationTime);
+	FileTimeToSystemTime(&lpFile.ftLastAccessTime, &accessTime);
+	FileTimeToSystemTime(&lpFile.ftLastWriteTime, &modificationTime);
+
+	sprintf_s(buffer, "%ws\t%d.%d.%d %d:%d \t%d.%d.%d %d:%d \t%d.%d.%d %d:%d \n", lpFile.cFileName, 
+		creationTime.wDay, creationTime.wMonth, creationTime.wYear, creationTime.wHour, creationTime.wMinute,
+		accessTime.wDay, accessTime.wMonth, accessTime.wYear, accessTime.wHour, accessTime.wMinute,
+		modificationTime.wDay, modificationTime.wMonth, modificationTime.wYear, modificationTime.wHour, modificationTime.wMinute);
 
 	DWORD all;
 	SetFilePointer(hFileResult, 0, NULL, FILE_END);
@@ -28,9 +36,16 @@ int main()
 	while (FindNextFile(hFileMask, &lpFile)) {
 		
 		//write other files
+		FileTimeToSystemTime(&lpFile.ftCreationTime, &creationTime);
+		FileTimeToSystemTime(&lpFile.ftLastAccessTime, &accessTime);
+		FileTimeToSystemTime(&lpFile.ftLastWriteTime, &modificationTime);
+
 		DWORD nb;
-		char buffer[50];
-		sprintf_s(buffer, "%ws %d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
+		char buffer[100];
+		sprintf_s(buffer, "%ws %d.%d.%d %d:%d \t%d.%d.%d %d:%d \t%d.%d.%d %d:%d \n", lpFile.cFileName, 
+			creationTime.wDay, creationTime.wMonth, creationTime.wYear, creationTime.wHour, creationTime.wMinute,
+			accessTime.wDay, accessTime.wMonth, accessTime.wYear, accessTime.wHour, accessTime.wMinute,
+			modificationTime.wDay, modificationTime.wMonth, modificationTime.wYear, modificationTime.wHour, modificationTime.wMinute);
 		
 		SetFilePointer(hFileResult, 0, NULL, FILE_END);
 		WriteFile(hFileResult, buffer, strlen(buffer), &nb, NULL);
