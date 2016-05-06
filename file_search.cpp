c5dac1630b1c97a9029bae88c96babfbced65981
qwe <artyom366@inbox.lv>
2016-05-06 20:47:35 +0300

    Update file_search.cpp

--- file_search.cpp
+++ file_search.cpp
@@ -20,35 +20,41 @@ int main()
 	char buffer[50];
 	sprintf_s(buffer, "%ws\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
 
-	DWORD nb;
+	DWORD all;
 	SetFilePointer(hFileResult, 0, NULL, FILE_END);
-	WriteFile(hFileResult, buffer, strlen(buffer), &nb, NULL);
+	WriteFile(hFileResult, buffer, strlen(buffer), &all, NULL);
 	
 	//find other files
 	while (FindNextFile(hFileMask, &lpFile)) {
-
+		
 		//write other files
+		DWORD nb;
 		char buffer[50];
-		sprintf_s(buffer, "%ws\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
+		sprintf_s(buffer, "%ws %d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
+		
 		SetFilePointer(hFileResult, 0, NULL, FILE_END);
 		WriteFile(hFileResult, buffer, strlen(buffer), &nb, NULL);
+
+		//collect all the bytes written
+		all = all + nb;
 	}
-
-	//read file names from result file
-
 
 	CloseHandle(hFileResult);
-
-
-	hFileResult = CreateFile(_TEXT("a.txt"), GENERIC_READ, 0, NULL,OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	char m;
-	ReadFile(hFileResult, &m, sizeof(char), &nb, NULL);
 	
-	while (nb != 0)
-	{
-		cout << m << endl;
-		ReadFile(hFileResult, &m, sizeof(char), &nb, NULL);
+	//read file names from result file
+	SetCurrentDirectory(_TEXT("D:\\TSI\\Sys\\c_lang_sys\\lab\\files"));
+	hFileResult = CreateFile(_TEXT("a.txt"), GENERIC_READ, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	
+	while (all != 0) {
+		DWORD read;
+		char buff;
+		ReadFile(hFileResult, &buff, sizeof(char), &read, NULL);
+		cout << buff;
+		all = all - read;
 	}
+
+	
+	
 	
 	CloseHandle(hFileResult);
 	system("PAUSE");
