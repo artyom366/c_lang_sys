b3babfdc5241996d41f28ea1806986d56f7f5b62
qwe <artyom366@inbox.lv>
2016-05-06 20:15:07 +0300

    Update file_search.cpp

--- file_search.cpp
+++ file_search.cpp
@@ -1,68 +1,57 @@
 #include<iostream>
 #include<windows.h>
 #include<tchar.h>
-
 using namespace std;
-
-
-void main()
+int main()
 {
 	HANDLE hFileResult;
-	hFileResult = CreateFile(_TEXT("files.dat."), GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-
-	HANDLE hFileBase;
-	WIN32_FIND_DATA lpFile;
-	SYSTEMTIME lpSys;
+	hFileResult = CreateFile(_TEXT("a.txt"), GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	
 	SetCurrentDirectory(_TEXT("C:\\"));
-	hFileBase = FindFirstFile(_TEXT("*.*"), &lpFile);
 
-	SetFilePointer(hFileResult, 4, NULL, FILE_END);
+	//find first file
+	SYSTEMTIME lpSys;
+	WIN32_FIND_DATA lpFile;
+	HANDLE hFileMask = FindFirstFile(_TEXT("*.*"), &lpFile);
 
-	if (hFileBase != INVALID_HANDLE_VALUE) {
+	FileTimeToSystemTime(&lpFile.ftLastWriteTime, &lpSys);
 
-		if (lpFile.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-		{
-			//printf("DIRECTORY\t");
-		}
-		else
-		{
-			//printf("FILE\t\t");
-		}
+	//write first file 
+	char buffer[50];
+	sprintf_s(buffer, "%ws\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
 
-		FileTimeToSystemTime(&lpFile.ftLastWriteTime, &lpSys);
-		//printf_s("%ws\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
+	DWORD nb;
+	SetFilePointer(hFileResult, 0, NULL, FILE_END);
+	WriteFile(hFileResult, buffer, strlen(buffer), &nb, NULL);
+	
+	//find other files
+	while (FindNextFile(hFileMask, &lpFile)) {
 
+		//write other files
 		char buffer[50];
-		sprintf_s(buffer, "%ws\t\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
-
-		WriteFile(hFileResult, buffer, sizeof(buffer), NULL, NULL);
-
-
-	} 
-	else {
-		return;
+		sprintf_s(buffer, "%ws\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
+		SetFilePointer(hFileResult, 0, NULL, FILE_END);
+		WriteFile(hFileResult, buffer, strlen(buffer), &nb, NULL);
 	}
 
-	while (FindNextFile(hFileBase, &lpFile))
+	//read file names from result file
+
+
+	CloseHandle(hFileResult);
+
+
+	hFileResult = CreateFile(_TEXT("a.txt"), GENERIC_READ, 0, NULL,OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	char m;
+	ReadFile(hFileResult, &m, sizeof(char), &nb, NULL);
+	
+	while (nb != 0)
 	{
-		if (lpFile.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-		{
-			printf("DIRECTORY\t");
-
-		}
-		else
-		{
-			printf("FILE\t\t");
-		}
-
-		FileTimeToSystemTime(&lpFile.ftLastWriteTime, &lpSys);
-
-		printf_s("%ws\t\t%d.%d.%d %d:%d\n", lpFile.cFileName, lpSys.wDay, lpSys.wMonth, lpSys.wYear, lpSys.wHour, lpSys.wMinute);
-
+		cout << m << endl;
+		ReadFile(hFileResult, &m, sizeof(char), &nb, NULL);
 	}
-
-	FindClose(hFileBase);
+	
+	CloseHandle(hFileResult);
 	system("PAUSE");
+	
+	return 0;
 }
-}
-
